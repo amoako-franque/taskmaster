03391cde45637417a5a64e4de07d1c9074869bcd
Franque Armoako <frank.amoako-dev@outlook.com>
2024-07-10 17:22:10 +0000

    basic server added

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("hi there")
+const express = require("express")
+require("dotenv").config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/api", require("./routes/authRoutes"))
+app.get("/", (req, res) => {
+	res.send("hello from simple server :)")
+})
+
+app.listen(port, () =>
+	console.log(`> Server is up and running on port : http://localhost:${port}`)
+)
