de8b027071ee3e3e0c81a670ad2d3ff53684399b
sterlen7 <infosterlen@gmail.com>
2024-07-26 00:27:08 +0000

    minor

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
 const user =  require ('../models/userModel')
+const expressAsyncHandler = require("express-async-handler")
 const bcrypt = require ('bcrypt')
 const jwt = require('jsonwebtoken')
 
 
-
-exports.userRegister = async (req,res) => {
-    const { firstname, lastname, username, email, password, phoneNumber, address, city, bio, country } = req.body;
+exports.userRegister = expressAsyncHandler(async (req,res) => {
+    const { firstname, lastname, username, email, password, phoneNumber, address, city, bio, country,roles } = req.body;
 
     try{
         const hashedPassword = await bcrypt.hash(password,10)
@@ -27,7 +27,8 @@ exports.userRegister = async (req,res) => {
             address,
             city,
             bio,
-            country
+            country,
+            roles
         });
 
        const successful = await newUser.save()
@@ -39,7 +40,58 @@ exports.userRegister = async (req,res) => {
     }catch(error){
         console.error(error)
         res.status(500).json({ message: 'Server error' },error);
-
     }
-}
+})
 
+
+exports.userLogin = expressAsyncHandler(async (req,res) =>{
+    const {username,email,password} = req.body
+
+    try{
+        if(!username && !email){
+            return res.status(400).json({msg:"Provide email or username"})
+        }
+
+        if(!password){
+            return res.status(400).json({msg:"provide password!"})
+        }
+        const userExist = await user.findOne({$or:[{username},{email}]})
+
+        if(!userExist){
+            return res.status(400).json({msg:"User not found! sign up"})
+        }
+        const correctPassword = await bcrypt.compare(password,userExist.password)
+
+        if(!correctPassword){
+            return res.status(401).json({msg:"Wrong password"})
+        }
+
+        const token = jwt.sign({userId :user._id},process.env.JWT_ACCESS_SECRET_KEY,{expiresIn:'4000s'})
+
+        res.status(200).json({
+            msg:"Login success",
+            token})
+
+    }catch(error){
+        console.error(error)
+        res.status(500).json({msg:"Server error",error})
+    }
+})
+
+exports.userLogout = expressAsyncHandler(async (req,res) => {
+    try{
+        const token = req.headers.authorization?.split(' ')[1]
+
+        if(token){
+            console.log(`Token ${token} added to blacklist`)
+        }
+
+        res.status(200).json({msg:"Log out successful"})
+
+    }catch(error){
+        console.error(error)
+        res.status(500).json({msg:"Error logging out",error})
+    }
+})
+
+exports.resetP
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { userRegister } = require('../controllers/userController');
+const { userRegister, userLogin, userLogout } = require('../controllers/userController');
 const userRoute = express.Router();
 
 
 userRoute.post('/user-register', userRegister)
+userRoute.post('/user-login',userLogin)
+userRoute.post('/logout',userLogout)
 
 
 module.exports = userRoute;
